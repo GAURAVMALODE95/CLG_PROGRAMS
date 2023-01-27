69e2aaa366dfee85c184ff18bdf4eb7fd0bf20fe
gaurav malode <65852778+GAURAVMALODE95@users.noreply.github.com>
2023-01-27 06:19:50 +0530

    Add files via upload

---
+++ 2nd_year/Genral/data_types_converion.cpp
@@ -0,0 +1,20 @@
+//conversion of int to float
+#include <iostream>
+using namespace std;
+int main(){
+    int a=1;
+    double b;
+    b=a;
+    cout<<b;
+    return 0;
+}
+//conversion of double to int
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    double b=99;
+    a=b;
+    cout<<a;
+    return 0; 
+}
